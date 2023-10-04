8ae8a4358bc59d5baccbae3423fec4b29d4d684d
Balaji <117513456+balaji170201@users.noreply.github.com>
2023-10-04 17:24:53 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Card from './Components/card';
+import Card2 from './Components/card2';
+import Card3 from './Components/card3';
+
+function App() {
+  return (
+    <div className="App">
+      <Card></Card>
+      <Card2></Card2>
+      <Card3></Card3>
+    </div>
+  );
+}
+
+export default App;
